a816745bcca830c9762022cfa373d201c500cd31
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2024-06-28 01:30:34 +0400

    1

---
+++ greetings.go
@@ -0,0 +1,25 @@
+package main
+
+import ("fmt")
+
+func main() {
+	// var student string = "John"
+	// var student2 = "Jane"
+	// x := 2
+
+	// fmt.Println(student)
+	// fmt.Println(student2)
+	// fmt.Println(x)
+
+	// var a string
+	// var b int
+	// var c bool
+  
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+
+	var student1 string
+	student1 = "John"
+	fmt.Println(student1)
+}
