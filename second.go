cf6b1a4cd52adc8c715a62d32b66173fead1b25e
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2024-07-14 15:00:43 +0400

    switch/case statement

--- second.go
+++ second.go
@@ -24,7 +24,7 @@ func main () {
 	// loop
 
 
-    array := [8]int{1, 2, 3, 4, 5, 6, 7, 8}
+    array := [6]int{1, 2, 3, 4, 5, 6,}
 	
 	// for i:=1 ; i<= len(array); i++{
 	// 	if i % 2 == 0{
@@ -44,6 +44,12 @@ func main () {
             fmt.Println("Two")
         case 3:
             fmt.Println("Three")
+		case 4: 
+			fmt.Println("Four")
+		case 5:
+			fmt.Println("Five")
+		case 6:
+			fmt.Println("Six")
         default:
             fmt.Println("Other number")
         }
