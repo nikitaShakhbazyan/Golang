0eb814b397cbc98462ebc700826266ea71d4b868
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2024-11-28 15:44:16 +0400

    f

--- second.go
+++ second.go
@@ -2,56 +2,18 @@ package main
 
 import "fmt"
 
+var node, golang, javs bool
+
+var array1 []int
+
 func main () {
-	// fmt.Println(1)
-	// fmt.Println(2)
-	// fmt.Println(3)
-	// fmt.Println(4)
-	// fmt.Println(5)
-	// fmt.Println(5)
-	// fmt.Println(6)
-	// fmt.Println(7)
-	// fmt.Println(8)
-	// fmt.Println(9)
-	// fmt.Println(10)
+	fmt.Println("hi")
+	var x int
+	fmt.Println(x,node)
 
-	// i := 1
-
-	// for i<=10 {
-	// 	fmt.Println(i)
-	// 	i = i + 1
-	// }
-	// loop
-
-
-    array := [6]int{1, 2, 3, 4, 5, 6,}
-	
-	// for i:=1 ; i<= len(array); i++{
-	// 	if i % 2 == 0{
-	// 		fmt.Println(i,"EVEN")
-	// 	} else {
-	// 		fmt.Println(i,"odd")
-	// 	}
-	// }
-
-	for i := 0; i < len(array); i++ {
-        switch array[i] {
-        case 0:
-            fmt.Println("Zero")
-        case 1:
-            fmt.Println("One")
-        case 2:
-            fmt.Println("Two")
-        case 3:
-            fmt.Println("Three")
-		case 4: 
-			fmt.Println("Four")
-		case 5:
-			fmt.Println("Five")
-		case 6:
-			fmt.Println("Six")
-        default:
-            fmt.Println("Other number")
-        }
-    }
+	for i := 0 ; i<10; i++{
+		array1 = append(array1, i)
+	}
+	array1= append([]int{15}, array1...)
+	fmt.Println(array1)
 }
