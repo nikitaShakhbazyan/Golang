2e87b9e07bad61c8e23c11ac62bf9efb4ce72299
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2024-12-03 12:33:12 +0400

    Functions and return types

--- lessons/main.go
+++ lessons/main.go
@@ -3,20 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
-	// type can be declared in this case like this
-	card = "DIDID"
-	var example float64 = 3132.22
-	var integer int = 2003
-	var logic bool = false
-	fmt.Println(card)
-	fmt.Println(example)
-	fmt.Println(integer)
-	fmt.Println(logic)
+	// var card string = "Ace of Spades"
+	// // type can be declared in this case like this
+	// card = "DIDID"
+	// var example float64 = 3132.22
+	// var integer int = 2003
+	// var logic bool = false
+	// fmt.Println(card)
+	// fmt.Println(example)
+	// fmt.Println(integer)
+	// fmt.Println(logic)
 
-	card2 := "IDK"
-	// and here we can write value without declaring type but if we write another type to the variable there will be an error
-	// card2 = false
-	fmt.Println(card2)
+	// card2 := "IDK"
+	// // and here we can write value without declaring type but if we write another type to the variable there will be an error
+	// // card2 = false
+	// fmt.Println(card2)
 
+	newCardVar := newCard()
+	fmt.Println(newCardVar)
+}
+
+
+
+func newCard() int {
+	return 105
 }
