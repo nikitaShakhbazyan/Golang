55a51e76d33ac83b1678b3c07fdfbc8b5d1137d2
Nikita Shakhbazyan <129982391+nikitaShakhbazyan@users.noreply.github.com>
2025-03-20 10:17:33 +0400

    test

--- go-demo/main.go
+++ go-demo/main.go
@@ -7,8 +7,11 @@ import (
 
 
 func main(){
-	var userHeight = 1.8
-	var userKG float64 = 100
+	 userHeight := 1.8
+	var userKG float64 = 100 // can't use userKG float64 := 100 - point a type needs to be declared as VAR or
+	// var userKG float64
+	// userKG = 100  OR
+	// var userHeight,userKG float64 = 1.8,100
 	var IMT = userKG / math.Pow(userHeight,2)
 
 	fmt.Print(IMT)
